06e3fcaa0687bf0e916ede558970452474161c63
abraham <apolichenco@gmail.com>
2022-06-09 20:01:35 -0400

    commit tree a640a449ecedeec8c9902ffb28fa701658f993b6

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const splitWord= word.split('')
+  const flippedWord = splitWord.reverse();
+  const wordJoinedBack = flippedWord.join('')
+  if (word === wordJoinedBack) {
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  Break up the word
+  flip it
+  join it back
+  compare with the original word
 */
+
 
 /*
   Add written explanation of your solution here
+  We took the word, and split each letter into an array
+  We flipped the array
+  We put them back together, and made it a new variable
+  We compared the original word and the new variable to see if they're the same
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +37,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("kayak"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("moshiach"));
 }
 
 module.exports = isPalindrome;
